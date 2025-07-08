14f9652ca9189a3aac76d91f5b8c183a004289d6
Rahul Kumar <therahulkumar135@gmail.com>
2025-07-09 01:10:43 +0530

    Added ProductShowcase Component at page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import ProductShowcase from "@/components/ProductShowcase"
+
 export default function Home() {
   return (
-    <main className="p-6">
-      <h1 className="text-4xl font-bold">Welcome to MathToWord Tools</h1>
-      <p className="mt-2 text-lg text-gray-600">
-        Explore all the tools we’ve built to help make working with math and documents easier.
-      </p>
+    <main>
+      <ProductShowcase />
     </main>
-  );
+  )
 }
---
+++ src/components/ProductShowcase.tsx
@@ -0,0 +1,11 @@
+import ProductHeader from "./ProductHeader"
+import ProductGrid from "./ProductGrid"
+
+export default function ProductShowcase() {
+  return (
+    <div>
+     <ProductHeader />
+     <ProductGrid />
+    </div>
+  )
+}
